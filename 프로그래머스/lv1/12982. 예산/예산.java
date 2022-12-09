430711ed5b7db1bdc44a8292006eb8ce1a9a7e1c
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-09 09:15:19 +0900

    [level 1] Title: 예산, Time: 0.33 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int sum = 0;
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if (sum > budget) return i;
+            if (sum == budget) return i+1;
+        }
+        return d.length;
+    }
+}
