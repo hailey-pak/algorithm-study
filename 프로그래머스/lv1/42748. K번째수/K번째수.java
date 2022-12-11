cd1c0f2994365f165b35882203cdee8cea3463c8
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-11 17:06:34 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int n = 0; n < answer.length; n++) {
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
+
+            int[] arr = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(arr);
+            answer[n] = arr[k-1];
+        }
+        return answer;
+    }
+}
