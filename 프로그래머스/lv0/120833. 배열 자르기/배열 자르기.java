fb6e0b912dea81eb9ffae7d7bd82380fb9dbea7b
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-11 19:35:39 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,9 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
         public int[] solution(int[] numbers, int num1, int num2) {
-            int[] answer = new int[num2-num1+1];
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = numbers[i+num1];
-            }
-            return answer;
+            return Arrays.copyOfRange(numbers, num1, num2 + 1);
         }
 }
