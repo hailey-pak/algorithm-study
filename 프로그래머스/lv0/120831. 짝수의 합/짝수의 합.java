fcd8716c59ffe7ae27dc08b588a7dbafdf2a4385
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-11 19:28:05 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        if (n < 2) return 0;
+        for (int i = 2; i <= n; i = i + 2) {
+            sum += i;
+        }
+        return sum;
+    }
+}
