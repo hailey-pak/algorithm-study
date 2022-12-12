8dce47e74cbe4845a5fdfc812e1b25743b23ad45
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-12 16:02:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.44 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,32 +1,10 @@
-import java.util.HashMap;
-
 class Solution {
     public int solution(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("zero", 0);
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        map.put("four", 4);
-        map.put("five", 5);
-        map.put("six", 6);
-        map.put("seven", 7);
-        map.put("eight", 8);
-        map.put("nine", 9);
+       String[] letter = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        StringBuilder key = new StringBuilder();
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLetter(s.charAt(i))) {
-                key.append(s.charAt(i));
-                if (map.containsKey(key.toString())) {
-                    ans.append(map.get(key.toString()));
-                    key.setLength(0);
-                }
-            } else {
-                ans.append(s.charAt(i));
-            }
+        for (int i = 0; i < letter.length; i++) {
+           s = s.replaceAll(letter[i], Integer.toString(i));
         }
-        return Integer.parseInt(ans.toString());
+        return Integer.parseInt(s);
     }
 }
