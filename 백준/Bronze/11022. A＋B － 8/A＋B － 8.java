bf22b69c96863dc879cb726e9346d6cd766e7ae1
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-13 23:50:17 +0900

    [Bronze V] Title: A+B - 8, Time: 168 ms, Memory: 16536 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            String[] in = br.readLine().split(" ");
+            bw.write("Case #" + i + ": " + in[0] + " + " + in[1] + " = ");
+            bw.write(String.valueOf(Integer.parseInt(in[0]) + Integer.parseInt(in[1])));
+            if (i!=t) bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
