012c5def731c9246bcce2740e792f0a4ca88c2c8
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-13 23:26:35 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        if ( (a%4==0 && a%100!=0) || a%400==0) {
+            System.out.print(1);
+        }else {
+            System.out.print(0);
+        }
+    }
+}
