1657d1e5f2d85547aa6331ac59d7cf1d2fdb4b0e
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-13 23:22:02 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,11 +9,11 @@ public class Main {
 		String gr = "F";
 		if (in>=90) {
 			gr = "A";
-		} else if (in>=80 && in<90) {
+		} else if (in>=80) {
 			gr = "B";
-		} else if (in>=70 && in<80) {
+		} else if (in>=70) {
 			gr = "C";
-		} else if (in>=60 && in<70) {
+		} else if (in>=60) {
 			gr = "D";
 		}
 		System.out.print(gr);
