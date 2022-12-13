d5ead8b54caeedb8608bf92509225c90203abd0f
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-13 16:50:07 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,28 +1,21 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] all = new int[n+1];
-        for (int i = 1; i <= n; i++) {
-            all[i] = 1;
-        }
+        int[] all = new int[n+2];
         for (int i : lost) all[i]--;
         for (int i : reserve) all[i]++;
 
-        int cnt = 0;
+        int cnt = n;
         for (int i = 1; i <= n; i++) {
-            if (all[i] == 0) {
-                if (i != 1 && all[i-1] > 1) {
+            if (all[i] == -1) {
+                if (all[i-1] > 0) {
                     all[i]++;
                     all[i-1]--;
-                    cnt++;
-                    continue;
-                }
-                if (i != n && all[i+1] > 1) {
+                } else if (all[i+1] > 0) {
                     all[i]++;
                     all[i+1]--;
-                    cnt++;
+                } else {
+                    cnt--;
                 }
-            } else {
-                cnt++;
             }
         }
         return cnt;
