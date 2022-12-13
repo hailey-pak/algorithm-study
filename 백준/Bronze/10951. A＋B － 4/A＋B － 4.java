75c3478c6a3a2a9b1f96bec3caf8182e8813bdb9
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-14 00:00:00 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String in;
+
+        while ((in = br.readLine()) != null) {
+            st = new StringTokenizer(in, " ");
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
