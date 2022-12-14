0dc380c0b15c051d36ebc0261dcd6432e0c98879
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-14 23:30:10 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n, s = 0;
+        while ( (n = System.in.read() - 59) > -1)
+            s += (n - n / 24 - n / 31) / 3 + 1;
+        System.out.print(s);
+    }
+}
