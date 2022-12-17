b3cca75f14d6ee208a80fae716723021a5234c77
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-18 01:16:49 +0900

    [level 0] Title: A로 B 만들기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String before, String after) {
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < before.length(); i++) {
+            sum1 += before.charAt(i);
+            sum2 += after.charAt(i);
+        }
+        return sum1 == sum2 ? 1 : 0;
+    }
+}
