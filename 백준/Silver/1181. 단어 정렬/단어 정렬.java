e67a3d8377bd84faa5805e3a0f03f8c43e27d278
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-19 21:36:55 +0900

    [Silver V] Title: 단어 정렬, Time: 376 ms, Memory: 24076 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = br.readLine();
+        }
+
+        Arrays.sort(str, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            } else {
+                return o1.length() - o2.length();
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && str[i-1].equals(str[i])) continue;
+            bw.write(str[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
