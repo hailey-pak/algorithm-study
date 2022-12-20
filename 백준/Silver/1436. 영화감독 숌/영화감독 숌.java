b3a5d608f5ef54dd97b6402643f3b9b6bcc592db
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-20 21:59:25 +0900

    [Silver V] Title: 영화감독 숌, Time: 388 ms, Memory: 89648 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        int cnt = 1;
+        int num = 666;
+        while (cnt < n) {
+            num ++;
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
+            }
+        }
+        System.out.print(num);
+    }
+}
