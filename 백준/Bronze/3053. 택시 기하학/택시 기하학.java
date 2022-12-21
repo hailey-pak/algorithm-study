6997e047323efe1482634ef546829ef6750667ee
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-21 20:32:50 +0900

    [Bronze III] Title: 택시 기하학, Time: 224 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double r = new Scanner(System.in).nextDouble();
+        System.out.println(Math.PI * r * r);
+        System.out.println(2 * r * r);
+    }
+}
