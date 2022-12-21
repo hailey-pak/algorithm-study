8623950f0254dae2abae4d349788c9c0efe9f339
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-21 20:20:41 +0900

    [Silver IV] Title: 듣보잡, Time: 316 ms, Memory: 26356 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if (set.contains(str)) {
+                list.add(str);
+            }
+        }
+
+        list.sort(Comparator.naturalOrder());
+        bw.write(list.size()+"\n");
+        for (String str : list) {
+            bw.write(str + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
