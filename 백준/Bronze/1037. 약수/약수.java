8a6538c743ccdf2006cc07d33a4a0c0639ac39b8
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-22 23:26:41 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,16 +1,24 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        int[] arr = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int max = Arrays.stream(arr).max().getAsInt();
-        int min = Arrays.stream(arr).min().getAsInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 2;
+        int min = 1000000;
+        for (int i : arr) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
         System.out.print(max * min);
         br.close();
     }
