2ac21907a3d87548c7158b9eed01fc3d977e5f81
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-28 11:07:49 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.18 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-       int[] arr = new int[number];
-        arr[0] = 1;
-        for (int i = 2; i <= number; i++) {
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    arr[i-1] = arr[i-1] + 2;
-                    if (j == Math.sqrt(i)) {
-                        arr[i-1] = arr[i-1] -1;
-                    }
-                }
+       int[] arr = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                arr[i * j]++;
             }
         }
 
