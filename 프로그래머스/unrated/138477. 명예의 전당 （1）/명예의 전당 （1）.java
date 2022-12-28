f986a8dac28368cee8ba3accea1b731b891b8310
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-28 14:00:10 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.33 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> queue = new PriorityQueue();
+
+        for (int i = 0; i < score.length; i++) {
+            queue.add(score[i]);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+            answer[i] = queue.peek();
+        }
+        return answer;
+    }
+}
