0dc1a388f3eb411f4b556306b5d5f546e82aea96
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2022-12-30 13:43:16 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.80 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,12 +1,10 @@
-import java.util.StringTokenizer;
-
 class Solution {
     public String solution(String s) {
-        StringTokenizer st = new StringTokenizer(s);
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        while (st.hasMoreTokens()) {
-            int i = Integer.parseInt(st.nextToken());
+        String[] arr = s.split(" ");
+        int min = Integer.parseInt(arr[0]);
+        int max = Integer.parseInt(arr[0]);
+        for (String str : arr) {
+            int i = Integer.parseInt(str);
             min = Math.min(min, i);
             max = Math.max(max, i);
         }
