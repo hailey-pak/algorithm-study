ea37ef7712744f93e00eee8e146187282eab3d77
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-01-02 09:33:46 +0900

    [level 2] Title: 올바른 괄호, Time: 16.30 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+       if (s.charAt(0) == ')' || s.charAt(s.length()-1) == '(') {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
