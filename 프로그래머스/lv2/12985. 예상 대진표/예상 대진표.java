51ea9e33cc7a91a01ac36d7b9e6843f8c78bad65
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-01-09 14:36:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -4,16 +4,11 @@ class Solution {
 
         while (n != 1) {
             r++;
-
-             if (Math.abs(a - b) == 1) {
-                if (a / 2 != b / 2) {
-                   break;
-                }
-            }
-
             n >>= 1;
-            a = a % 2 == 0 ? a / 2 : a / 2 + 1;
-            b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+            a = a / 2 + a % 2;
+            b = b / 2 + b % 2;
+
+            if (a==b) break;
         }
         return r;
     }
