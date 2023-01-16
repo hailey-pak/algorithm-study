63a23b27673cc1678f0ebc20cfa7ad220e428a2c
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-01-16 15:10:02 +0900

    [level 2] Title: [1차] 캐시, Time: 4.69 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,23 +3,25 @@ import java.util.Queue;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+       if (cacheSize == 0) return cities.length * 5;
+        
         Queue<String> queue = new LinkedList<>();   //Queue -> FIFO(선입선출)
 
         int sum = 0;
         for (String s : cities) {
             s = s.toLowerCase();
-            
+
             if (queue.contains(s)) {
                 sum += 1;
                 queue.remove(s);        //해당 객체 제거 (제일 마지막에 다시 넣기 위해)
 
             } else {
                 sum += 5;
-                if (cacheSize != 0 && queue.size() == cacheSize) {
+                if (queue.size() == cacheSize) {
                     queue.remove();     //가장 먼저 들어온 객체 제거
                 }
             }
-            if (cacheSize != 0) queue.add(s);
+            queue.add(s);
         }
 
         return sum;
