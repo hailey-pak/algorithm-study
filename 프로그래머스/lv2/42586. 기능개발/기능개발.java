f27ee66df7d87feb995d440919082e01a7931fd1
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-01-18 15:16:09 +0900

    [level 2] Title: 기능개발, Time: 2.02 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,40 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new LinkedList<>();
-
+        int[] done = new int[100];
+        int day = -1;
         for (int i = 0; i < progresses.length; i++) {
-            int n = 0;
-            int p = progresses[i];
-            while (p < 100) {
-                p += speeds[i];
-                n++;
+            while (progresses[i] + (day * speeds[i]) < 100) {
+                day++;
             }
-            queue.add(n);
+            done[day]++;
         }
-
-        ArrayList<Integer> list = new ArrayList<>();
-        int n = 0, k = 0;
-        while (queue.size() != 0) {
-            if (n == 0) {
-                k = queue.poll();
-            } else {
-                if (queue.peek() <= k) {
-                    queue.poll();
-                } else {
-                    list.add(n);
-                    n = 0;
-                    k = queue.poll();
-                }
-            }
-            n++;
-        }
-        list.add(n);
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        return Arrays.stream(done).filter(i -> i!=0).toArray();
     }
 }
