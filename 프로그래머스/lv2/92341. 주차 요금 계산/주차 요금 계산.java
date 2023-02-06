2697cdac3eaba6da315bb1005ffc462d9839d1a5
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-02-06 14:04:22 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.42 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,49 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        HashMap<String, Integer> before = new HashMap<>();
-        HashMap<String, Integer> after = new HashMap<>();
-        TreeSet<String> cars = new TreeSet<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
         for (String record : records) {
             String[] str = record.split(" ");
 
             String car = str[1];
-            cars.add(car);
             int time = Integer.parseInt(str[0].substring(0, 2)) * 60 + Integer.parseInt(str[0].substring(3, 5));
-
             if (str[2].equals("IN")) {
-                before.put(car, time);
-
-            } else {
-                int sub = time - before.get(car);   //출차시간 - 입차시간
-                after.put(car, after.getOrDefault(car, 0) + sub);   //총 이용시간 누적해서 저장
-                before.remove(car);     //입-출차 끝난 차량 제거
+                time = time * -1;
             }
-        }
 
-        //마지막 출차가 없는 차량 23:59로 간주해서 계산
-        for (String car : before.keySet()) {
-            int time = 23 * 60 + 59;
-            int sub = time - before.get(car);
-            after.put(car, after.getOrDefault(car, 0) + sub);
+            map.put(car, map.getOrDefault(car, 0) + time);   //총 이용시간 누적해서 저장
         }
 
         //요금 계산
-        int[] answer = new int[after.size()];
-        ArrayList<String> carList = new ArrayList<>(cars);
+        int[] answer = new int[map.size()];
+        int i = 0;
 
-        for (int i = 0; i < answer.length; i++) {
-            double time = after.get(carList.get(i));  //차량번호로 총 이용 시간 가져옴
+        for (String car : map.keySet()) {
+            int time = map.get(car);
+            if (time < 1) time += 23 * 60 + 59;     //입차시간만 있으면 시간이 음수이므로 23:59분 더해줌
 
-            answer[i] = fees[1];                //기본 요금 부과
-
-            if (time >= fees[0] ) { //기본 시간보다 이용 시간이 길 때 추과 요금 부과
+            answer[i] = fees[1];                    //기본 요금 부과
+            if (time >= fees[0] ) {                 //기본 시간보다 이용 시간이 길 때 추과 요금 부과
                 time -= fees[0];
-                answer[i] += (int) (Math.ceil(time / fees[2]) * fees[3]);
+                answer[i] += (int) (Math.ceil((double)time / fees[2]) * fees[3]);
             }
+            i++;
         }
-
         return answer;
     }
 }
