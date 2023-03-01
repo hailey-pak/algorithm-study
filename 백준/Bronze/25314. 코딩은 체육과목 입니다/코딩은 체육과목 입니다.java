cccbe22d939bbf6ff84b2633a2d8e126eace1591
hailey-pak <112041005+hailey-pak@users.noreply.github.com>
2023-03-01 22:02:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = N / 4;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cnt; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+}
